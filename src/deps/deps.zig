6067653a2eb00a2891410dadc22a1295cb18b1ff
jackji <jichengde@gmail.com>
2022-12-09 10:22:40 +0800

    import deps as needed

--- src/deps/deps.zig
+++ src/deps/deps.zig
@@ -1,13 +1,39 @@
+const bos = @import("build_options");
+
 pub const stb = @import("stb/stb.zig");
-pub const imgui = @import("imgui/imgui.zig");
-pub const chipmunk = @import("chipmunk/chipmunk.zig");
-pub const nfd = @import("nfd/nfd.zig");
 pub const zmath = @import("zmath/src/zmath.zig");
 pub const zpool = @import("zpool/src/main.zig");
 pub const zjobs = @import("zjobs/src/zjobs.zig");
-pub const zaudio = @import("zaudio/src/zaudio.zig");
 pub const zmesh = @import("zmesh/src/main.zig");
 pub const znoise = @import("znoise/src/znoise.zig");
-pub const zbullet = @import("zbullet/src/zbullet.zig");
 pub const znetwork = @import("znetwork/src/main.zig");
-pub const ztracy = @import("ztracy/src/ztracy.zig");
+
+pub const imgui = if (bos.use_imgui)
+    @import("imgui/imgui.zig")
+else
+    null;
+
+pub const chipmunk = if (bos.use_chipmunk)
+    @import("chipmunk/chipmunk.zig")
+else
+    null;
+
+pub const nfd = if (bos.use_nfd)
+    @import("nfd/nfd.zig")
+else
+    null;
+
+pub const zaudio = if (bos.use_zaudio)
+    @import("zaudio/src/zaudio.zig")
+else
+    null;
+
+pub const zbullet = if (bos.use_zbullet)
+    @import("zbullet/src/zbullet.zig")
+else
+    null;
+
+pub const ztracy = if (bos.use_ztracy)
+    @import("ztracy/src/ztracy.zig")
+else
+    null;
