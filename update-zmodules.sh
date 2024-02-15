6f0e46817a8040388100938fea44406ad0e4e6e8
jack <jichengde@gmail.com>
2024-02-15 12:20:51 +0800

    update zmodules and scripts

--- update-zmodules.sh
+++ update-zmodules.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ZMODULES=../zig-gamedev/libs/
 
 rm -rf src/deps/system-sdk
@@ -5,6 +7,8 @@ cp -r $ZMODULES/system-sdk src/deps
 
 rm -rf src/deps/imgui/zgui
 cp -r $ZMODULES/zgui src/deps/imgui
+sed -i 's/\/\/\(#define ImDrawIdx unsigned int\)/\1/' src/deps/imgui/zgui/libs/imgui/imconfig.h
+sed -i 's/DrawIdx = u16/DrawIdx = u32/' src/deps/imgui/zgui/src/gui.zig
 
 rm -rf src/deps/zaudio
 cp -r $ZMODULES/zaudio src/deps
