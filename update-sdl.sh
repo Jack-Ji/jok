832be712a5627b91c25230ef88c8fd8e17d40fba
jack <jichengde@gmail.com>
2024-04-20 20:55:51 +0800

    fix update script

--- update-sdl.sh
+++ update-sdl.sh
@@ -8,4 +8,5 @@ fi
 rm -f $1.tar.gz
 wget https://github.com/jack-ji/sdl.zig/archive/$1.tar.gz
 zig fetch --save=sdl $1.tar.gz
+sed -i "s/$1/https:\/\/github.com\/jack-ji\/sdl.zig\/archive\/&/" build.zig.zon
 rm -f $1.tar.gz
