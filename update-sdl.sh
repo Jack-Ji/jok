42e9619be25356e626947334e949b7d3af789694
jack <jichengde@gmail.com>
2024-08-04 00:17:19 +0800

    preparing for lua script support

--- update-sdl.sh
+++ update-sdl.sh
@@ -8,5 +8,5 @@ fi
 rm -f $1.tar.gz
 wget https://github.com/jack-ji/sdl.zig/archive/$1.tar.gz
 zig fetch --save=sdl $1.tar.gz
-sed -i "s/$1/https:\/\/github.com\/jack-ji\/sdl.zig\/archive\/&/" build.zig.zon
+sed -i "s/$1\.tar\.gz/git+https:\/\/github.com\/jack-ji\/sdl.zig#$1/" build.zig.zon
 rm -f $1.tar.gz
