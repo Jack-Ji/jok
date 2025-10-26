4b66e380950f0b47775faefafd087c547053e9e9
jack <jichengde@gmail.com>
2025-10-26 20:41:45 +0800

    zobj: fix obj loader

--- src/vendor/zobj/utils.zig
+++ src/vendor/zobj/utils.zig
@@ -1,22 +1,20 @@
 const std = @import("std");
 
-pub fn LineIterator(comptime Reader: type) type {
-    return struct {
-        buffer: []u8,
-        reader: Reader,
+const LineIterator = struct {
+    buffer: []u8,
+    reader: *std.Io.Reader,
 
-        pub fn next(self: *@This()) !?[]const u8 {
-            var line = self.reader.takeDelimiterExclusive('\n') catch |err| {
-                if (err == error.EndOfStream) return null;
-                return err;
-            };
-            if (0 < line.len and line[line.len - 1] == '\r')
-                line = line[0 .. line.len - 1];
-            return line;
+    pub fn next(self: *@This()) !?[]const u8 {
+        if (try self.reader.takeDelimiter('\n')) |line| {
+            return if (0 < line.len and line[line.len - 1] == '\r')
+                line[0 .. line.len - 1]
+            else
+                line;
         }
-    };
-}
+        return null;
+    }
+};
 
-pub fn lineIterator(rdr: *std.Io.Reader, buffer: []u8) LineIterator(@TypeOf(rdr)) {
+pub fn lineIterator(rdr: *std.Io.Reader, buffer: []u8) LineIterator {
     return .{ .buffer = buffer, .reader = rdr };
 }
