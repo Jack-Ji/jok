Jack-Ji/jok

commit 8791d93815357cba008e3a5fcd6b018fe716d0b7

utils.easing: add custom data for extensibility

build.zig.zon
@@ -1,6 +1,6 @@
 .{
     .name = .jok,
-    .version = "0.89.4",
+    .version = "0.89.5",
     .fingerprint = 0x8204585c92cd7f34, // Changing this has security and trust implications.
     .paths = .{
         "README.md",

src/utils/easing.zig
@@ -60,13 +60,14 @@ pub fn EasingSystem(comptime T: type) type {
             from: T,
             to: T,
             finish: ?Finish,
+            data: ?*anyopaque,
         };
         pub const Finish = struct {
             callback: *const fn (EasingValue, data2: ?*anyopaque) void,
             data: ?*anyopaque = null,
         };
         pub const EasingSignal = signal.Signal(&.{*const EasingValue});
-        pub const EasingApplyFn = *const fn (x: f32, from: T, to: T) T;
+        pub const EasingApplyFn = *const fn (t: f32, from: T, to: T, data: ?*anyopaque) T;
         const VarState = enum {
             new,
             easing,
@@ -142,8 +143,8 @@ pub fn EasingSystem(comptime T: type) type {
 
                 // Apply ease function
                 ev.life_passed += delta_time;
-                const x = ev.easing_fn(@min(1.0, ev.life_passed / ev.life_total));
-                ev.v.* = ev.easing_apply_fn(x, ev.from, ev.to);
+                const t = ev.easing_fn(@min(1.0, ev.life_passed / ev.life_total));
+                ev.v.* = ev.easing_apply_fn(t, ev.from, ev.to, ev.data);
 
                 // Remove if it's done
                 if (ev.life_passed >= ev.life_total) {
@@ -165,6 +166,7 @@ pub fn EasingSystem(comptime T: type) type {
         pub const AddOption = struct {
             wait_time: f32 = 0,
             finish: ?Finish = null,
+            data: ?*anyopaque = null,
         };
         pub fn add(
             self: *Self,
@@ -195,6 +197,7 @@ pub fn EasingSystem(comptime T: type) type {
                 .from = from orelse v.*,
                 .to = to,
                 .finish = opt.finish,
+                .data = opt.data,
             };
             errdefer self.pool.destroy(ev);
             try self.search_tree.put(v, ev);
@@ -223,21 +226,21 @@ pub fn EasingSystem(comptime T: type) type {
     };
 }
 
-pub fn EaseScalar(comptime T: type) type {
+pub fn EaseScalarLinearly(comptime T: type) type {
     return struct {
-        pub fn ease(x: f32, from: T, to: T) T {
+        pub fn ease(t: f32, from: T, to: T, _: ?*anyopaque) T {
             return switch (T) {
-                f32 => @mulAdd(f32, to - from, x, from),
-                f64 => @mulAdd(f64, to - from, @as(f64, @floatCast(x)), from),
-                c_int, i8, i16, i32, i64 => @intFromFloat(@mulAdd(f32, @as(f32, @floatFromInt(to - from)), x, @as(f32, @floatFromInt(from)))),
-                u8, u16, u32, u64 => @intFromFloat(@mulAdd(f32, @as(f32, @floatFromInt(to)) - @as(f32, @floatFromInt(from)), x, @as(f32, @floatFromInt(from)))),
+                f32 => @mulAdd(f32, to - from, t, from),
+                f64 => @mulAdd(f64, to - from, @as(f64, @floatCast(t)), from),
+                c_int, i8, i16, i32, i64 => @intFromFloat(@mulAdd(f32, @as(f32, @floatFromInt(to - from)), t, @as(f32, @floatFromInt(from)))),
+                u8, u16, u32, u64 => @intFromFloat(@mulAdd(f32, @as(f32, @floatFromInt(to)) - @as(f32, @floatFromInt(from)), t, @as(f32, @floatFromInt(from)))),
                 else => unreachable,
             };
         }
     };
 }
 
-pub fn EaseVector(comptime N: u32, comptime T: type) type {
+pub fn EaseVectorLinearly(comptime N: u32, comptime T: type) type {
     const Vec = @Vector(N, T);
 
     return struct {
@@ -262,15 +265,15 @@ pub fn EaseVector(comptime N: u32, comptime T: type) type {
             return result;
         }
 
-        pub fn ease(x: f32, from: Vec, to: Vec) Vec {
+        pub fn ease(t: f32, from: Vec, to: Vec, _: ?*anyopaque) Vec {
             return switch (T) {
-                f32 => @mulAdd(Vec, to - from, @as(Vec, @splat(x)), from),
-                f64 => @mulAdd(Vec, to - from, @as(Vec, @splat(@as(f64, @floatCast(x)))), from),
+                f32 => @mulAdd(Vec, to - from, @as(Vec, @splat(t)), from),
+                f64 => @mulAdd(Vec, to - from, @as(Vec, @splat(@as(f64, @floatCast(t)))), from),
                 c_int, i8, i16, i32, i64, u8, u16, u32, u64 => BLK: {
                     const VecN = @Vector(N, f64);
                     const from_f64 = convert(T, f64, from);
                     const to_f64 = convert(T, f64, to);
-                    const result_f64 = @mulAdd(VecN, to_f64 - from_f64, @as(VecN, @splat(@as(f64, x))), from_f64);
+                    const result_f64 = @mulAdd(VecN, to_f64 - from_f64, @as(VecN, @splat(@as(f64, t))), from_f64);
                     break :BLK convert(f64, T, result_f64);
                 },
                 else => unreachable,
@@ -279,40 +282,40 @@ pub fn EaseVector(comptime N: u32, comptime T: type) type {
     };
 }
 
-pub fn easePoint(x: f32, from: jok.Point, to: jok.Point) jok.Point {
-    const es = EaseScalar(f32);
+pub fn easePointLinearly(t: f32, from: jok.Point, to: jok.Point, data: ?*anyopaque) jok.Point {
+    const es = EaseScalarLinearly(f32);
     return .{
-        .x = es.ease(x, from.x, to.x),
-        .y = es.ease(x, from.y, to.y),
+        .x = es.ease(t, from.x, to.x, data),
+        .y = es.ease(t, from.y, to.y, data),
     };
 }
 
-pub fn easeColor(x: f32, _from: jok.Color, _to: jok.Color) jok.Color {
-    const es = EaseVector(4, u8);
+pub fn easeColorLinearly(t: f32, _from: jok.Color, _to: jok.Color, _: ?*anyopaque) jok.Color {
+    const es = EaseVectorLinearly(4, u8);
     const from = @Vector(4, u8){ _from.r, _from.g, _from.b, _from.a };
     const to = @Vector(4, u8){ _to.r, _to.g, _to.b, _to.a };
-    const c = es.ease(x, from, to);
+    const c = es.ease(t, from, to);
     return .{ .r = c[0], .g = c[1], .b = c[2], .a = c[3] };
 }
 
-pub fn easeColorF(x: f32, _from: jok.ColorF, _to: jok.ColorF) jok.ColorF {
-    const es = EaseVector(4, f32);
+pub fn easeColorFLinearly(t: f32, _from: jok.ColorF, _to: jok.ColorF, _: ?*anyopaque) jok.ColorF {
+    const es = EaseVectorLinearly(4, f32);
     const from = @Vector(4, f32){ _from.r, _from.g, _from.b, _from.a };
     const to = @Vector(4, f32){ _to.r, _to.g, _to.b, _to.a };
-    const c = es.ease(x, from, to);
+    const c = es.ease(t, from, to);
     return .{ .r = c[0], .g = c[1], .b = c[2], .a = c[3] };
 }
 
-pub fn easeArray(x: f32, comptime n: usize, _from: [n]f32, _to: [n]f32) [n]f32 {
-    const es = EaseVector(n, f32);
-    const from: @Vector(4, f32) = _from;
-    const to: @Vector(4, f32) = _to;
-    return es.ease(x, from, to);
+pub fn easeArrayLinearly(t: f32, comptime N: usize, _from: [N]f32, _to: [N]f32, _: ?*anyopaque) [N]f32 {
+    const es = EaseVectorLinearly(N, f32);
+    const from: @Vector(N, f32) = _from;
+    const to: @Vector(N, f32) = _to;
+    return es.ease(t, from, to);
 }
 
 ///////////////////////////////////////////////////////////////////////
 ///
-/// Different kinds of easing functions (x goes from 0 to 1)
+/// Different kinds of easing functions (t goes from 0 to 1)
 ///
 ///////////////////////////////////////////////////////////////////////
 
@@ -354,219 +357,219 @@ pub fn getEasingFn(t: EasingType) EasingFn {
     };
 }
 
-pub fn linear(x: f32) f32 {
-    return x;
+pub fn linear(t: f32) f32 {
+    return t;
 }
 
 pub const sin = struct {
-    pub fn in(x: f32) f32 {
-        return 1 - math.cos((x * math.pi) / 2);
+    pub fn in(t: f32) f32 {
+        return 1 - math.cos((t * math.pi) / 2);
     }
 
-    pub fn out(x: f32) f32 {
-        return math.sin((x * math.pi) / 2);
+    pub fn out(t: f32) f32 {
+        return math.sin((t * math.pi) / 2);
     }
 
-    pub fn inOut(x: f32) f32 {
-        return -(math.cos(math.pi * x) - 1) / 2;
+    pub fn inOut(t: f32) f32 {
+        return -(math.cos(math.pi * t) - 1) / 2;
     }
 };
 
 pub const quad = struct {
-    pub fn in(x: f32) f32 {
-        return x * x;
+    pub fn in(t: f32) f32 {
+        return t * t;
     }
 
-    pub fn out(x: f32) f32 {
-        return 1 - (1 - x) * (1 - x);
+    pub fn out(t: f32) f32 {
+        return 1 - (1 - t) * (1 - t);
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (x < 0.5)
-            2 * x * x
+    pub fn inOut(t: f32) f32 {
+        return if (t < 0.5)
+            2 * t * t
         else
-            1 - math.pow(f32, -2 * x + 2, 2) / 2;
+            1 - math.pow(f32, -2 * t + 2, 2) / 2;
     }
 };
 
 pub const cubic = struct {
-    pub fn in(x: f32) f32 {
-        return x * x * x;
+    pub fn in(t: f32) f32 {
+        return t * t * t;
     }
 
-    pub fn out(x: f32) f32 {
-        return 1 - math.pow(f32, 1 - x, 3);
+    pub fn out(t: f32) f32 {
+        return 1 - math.pow(f32, 1 - t, 3);
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (x < 0.5)
-            4 * x * x * x
+    pub fn inOut(t: f32) f32 {
+        return if (t < 0.5)
+            4 * t * t * t
         else
-            1 - math.pow(f32, -2 * x + 2, 3) / 2;
+            1 - math.pow(f32, -2 * t + 2, 3) / 2;
     }
 };
 
 pub const quart = struct {
-    pub fn in(x: f32) f32 {
-        return x * x * x * x;
+    pub fn in(t: f32) f32 {
+        return t * t * t * t;
     }
 
-    pub fn out(x: f32) f32 {
-        return 1 - math.pow(f32, 1 - x, 4);
+    pub fn out(t: f32) f32 {
+        return 1 - math.pow(f32, 1 - t, 4);
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (x < 0.5)
-            8 * x * x * x * x
+    pub fn inOut(t: f32) f32 {
+        return if (t < 0.5)
+            8 * t * t * t * t
         else
-            1 - math.pow(f32, -2 * x + 2, 4) / 2;
+            1 - math.pow(f32, -2 * t + 2, 4) / 2;
     }
 };
 
 pub const quint = struct {
-    pub fn in(x: f32) f32 {
-        return x * x * x * x * x;
+    pub fn in(t: f32) f32 {
+        return t * t * t * t * t;
     }
 
-    pub fn out(x: f32) f32 {
-        return 1 - math.pow(f32, 1 - x, 5);
+    pub fn out(t: f32) f32 {
+        return 1 - math.pow(f32, 1 - t, 5);
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (x < 0.5)
-            16 * x * x * x * x * x
+    pub fn inOut(t: f32) f32 {
+        return if (t < 0.5)
+            16 * t * t * t * t * t
         else
-            1 - math.pow(f32, -2 * x + 2, 5) / 2;
+            1 - math.pow(f32, -2 * t + 2, 5) / 2;
     }
 };
 
 pub const expo = struct {
-    pub fn in(x: f32) f32 {
-        return if (math.approxEqAbs(f32, x, 0, math.floatEps(f32)))
+    pub fn in(t: f32) f32 {
+        return if (math.approxEqAbs(f32, t, 0, math.floatEps(f32)))
             0
         else
-            math.pow(f32, 2, 10 * x - 10);
+            math.pow(f32, 2, 10 * t - 10);
     }
 
-    pub fn out(x: f32) f32 {
-        return if (math.approxEqAbs(f32, x, 1, math.floatEps(f32)))
+    pub fn out(t: f32) f32 {
+        return if (math.approxEqAbs(f32, t, 1, math.floatEps(f32)))
             1
         else
-            1 - math.pow(f32, 2, -10 * x);
+            1 - math.pow(f32, 2, -10 * t);
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (math.approxEqAbs(f32, x, 0, math.floatEps(f32)))
+    pub fn inOut(t: f32) f32 {
+        return if (math.approxEqAbs(f32, t, 0, math.floatEps(f32)))
             0
-        else if (math.approxEqAbs(f32, x, 1, math.floatEps(f32)))
+        else if (math.approxEqAbs(f32, t, 1, math.floatEps(f32)))
             1
-        else if (x < 0.5)
-            math.pow(f32, 2, 20 * x - 10) / 2
+        else if (t < 0.5)
+            math.pow(f32, 2, 20 * t - 10) / 2
         else
-            (2 - math.pow(f32, 2, -20 * x + 10)) / 2;
+            (2 - math.pow(f32, 2, -20 * t + 10)) / 2;
     }
 };
 
 pub const circ = struct {
-    pub fn in(x: f32) f32 {
-        return 1.0 - math.sqrt(1.0 - math.pow(f32, x, 2));
+    pub fn in(t: f32) f32 {
+        return 1.0 - math.sqrt(1.0 - math.pow(f32, t, 2));
     }
 
-    pub fn out(x: f32) f32 {
-        return math.sqrt(1.0 - math.pow(f32, x - 1, 2));
+    pub fn out(t: f32) f32 {
+        return math.sqrt(1.0 - math.pow(f32, t - 1, 2));
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (x < 0.5)
-            (1.0 - math.sqrt(1.0 - math.pow(f32, 2 * x, 2))) / 2
+    pub fn inOut(t: f32) f32 {
+        return if (t < 0.5)
+            (1.0 - math.sqrt(1.0 - math.pow(f32, 2 * t, 2))) / 2
         else
-            (math.sqrt(1.0 - math.pow(f32, -2 * x + 2, 2)) + 1) / 2;
+            (math.sqrt(1.0 - math.pow(f32, -2 * t + 2, 2)) + 1) / 2;
     }
 };
 
 pub const back = struct {
-    pub fn in(x: f32) f32 {
+    pub fn in(t: f32) f32 {
         const c1 = 1.70158;
         const c3 = c1 + 1;
-        return c3 * x * x * x - c1 * x * x;
+        return c3 * t * t * t - c1 * t * t;
     }
 
-    pub fn out(x: f32) f32 {
+    pub fn out(t: f32) f32 {
         const c1 = 1.70158;
         const c3 = c1 + 1;
-        return 1.0 + c3 * math.pow(f32, x - 1, 3) + c1 * math.pow(f32, x - 1, 2);
+        return 1.0 + c3 * math.pow(f32, t - 1, 3) + c1 * math.pow(f32, t - 1, 2);
     }
 
-    pub fn inOut(x: f32) f32 {
+    pub fn inOut(t: f32) f32 {
         const c1 = 1.70158;
         const c2 = c1 * 1.525;
-        return if (x < 0.5)
-            (math.pow(f32, 2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+        return if (t < 0.5)
+            (math.pow(f32, 2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
         else
-            (math.pow(f32, 2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+            (math.pow(f32, 2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2;
     }
 };
 
 pub const elastic = struct {
-    pub fn in(x: f32) f32 {
+    pub fn in(t: f32) f32 {
         const c4 = (2.0 * math.pi) / 3.0;
-        return if (math.approxEqAbs(f32, x, 0, math.floatEps(f32)))
+        return if (math.approxEqAbs(f32, t, 0, math.floatEps(f32)))
             0
-        else if (math.approxEqAbs(f32, x, 1, math.floatEps(f32)))
+        else if (math.approxEqAbs(f32, t, 1, math.floatEps(f32)))
             1
         else
-            -math.pow(f32, 2, 10 * x - 10) * math.sin((x * 10 - 10.75) * c4);
+            -math.pow(f32, 2, 10 * t - 10) * math.sin((t * 10 - 10.75) * c4);
     }
 
-    pub fn out(x: f32) f32 {
+    pub fn out(t: f32) f32 {
         const c4 = (2.0 * math.pi) / 3.0;
-        return if (math.approxEqAbs(f32, x, 0, math.floatEps(f32)))
+        return if (math.approxEqAbs(f32, t, 0, math.floatEps(f32)))
             0
-        else if (math.approxEqAbs(f32, x, 1, math.floatEps(f32)))
+        else if (math.approxEqAbs(f32, t, 1, math.floatEps(f32)))
             1
         else
-            math.pow(f32, 2, -10 * x) * math.sin((x * 10 - 0.75) * c4) + 1;
+            math.pow(f32, 2, -10 * t) * math.sin((t * 10 - 0.75) * c4) + 1;
     }
 
-    pub fn inOut(x: f32) f32 {
+    pub fn inOut(t: f32) f32 {
         const c5 = (2.0 * math.pi) / 4.5;
-        return if (math.approxEqAbs(f32, x, 0, math.floatEps(f32)))
+        return if (math.approxEqAbs(f32, t, 0, math.floatEps(f32)))
             0
-        else if (math.approxEqAbs(f32, x, 1, math.floatEps(f32)))
+        else if (math.approxEqAbs(f32, t, 1, math.floatEps(f32)))
             1
-        else if (x < 0.5)
-            -(math.pow(f32, 2, 20 * x - 10) * math.sin((20 * x - 11.125) * c5)) / 2
+        else if (t < 0.5)
+            -(math.pow(f32, 2, 20 * t - 10) * math.sin((20 * t - 11.125) * c5)) / 2
         else
-            (math.pow(f32, 2, -20 * x + 10) * math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+            (math.pow(f32, 2, -20 * t + 10) * math.sin((20 * t - 11.125) * c5)) / 2 + 1;
     }
 };
 
 pub const bounce = struct {
-    pub fn in(x: f32) f32 {
-        return 1 - out(1 - x);
+    pub fn in(t: f32) f32 {
+        return 1 - out(1 - t);
     }
 
-    pub fn out(x: f32) f32 {
+    pub fn out(t: f32) f32 {
         const n1 = 7.5625;
         const d1 = 2.75;
-        if (x < 1.0 / d1) {
-            return n1 * x * x;
-        } else if (x < 2.0 / d1) {
-            const _x = x - 1.5 / d1;
+        if (t < 1.0 / d1) {
+            return n1 * t * t;
+        } else if (t < 2.0 / d1) {
+            const _x = t - 1.5 / d1;
             return n1 * _x * _x + 0.75;
-        } else if (x < 2.5 / d1) {
-            const _x = x - 2.25 / d1;
+        } else if (t < 2.5 / d1) {
+            const _x = t - 2.25 / d1;
             return n1 * _x * _x + 0.9375;
         } else {
-            const _x = x - 2.625 / d1;
+            const _x = t - 2.625 / d1;
             return n1 * _x * _x + 0.984375;
         }
     }
 
-    pub fn inOut(x: f32) f32 {
-        return if (x < 0.5)
-            (1.0 - out(1.0 - 2.0 * x)) / 2.0
+    pub fn inOut(t: f32) f32 {
+        return if (t < 0.5)
+            (1.0 - out(1.0 - 2.0 * t)) / 2.0
         else
-            (1.0 + out(2.0 * x - 1.0)) / 2.0;
+            (1.0 + out(2.0 * t - 1.0)) / 2.0;
     }
 };

examples/easing.zig
@@ -30,7 +30,7 @@ const EasingBlock = struct {
         try es.add(
             &self.pos,
             easing.getEasingFn(@enumFromInt(@as(u8, @intCast(self.id)))),
-            easing.easePoint,
+            easing.easePointLinearly,
             2,
             .{
                 .x = 150,

commit a42faa315f04bea0df9a99271236519d99b907c9

fix regression caused by 8791d93

build.zig.zon
@@ -1,6 +1,6 @@
 .{
     .name = .jok,
-    .version = "0.89.6",
+    .version = "0.89.7",
     .fingerprint = 0x8204585c92cd7f34, // Changing this has security and trust implications.
     .paths = .{
         "README.md",

examples/hello.zig
@@ -89,7 +89,7 @@ pub fn event(ctx: jok.Context, e: jok.Event) !void {
                 try point_easing_system.add(
                     &screenshot_pos,
                     easing.getEasingFn(.in_out_circ),
-                    easing.easePoint,
+                    easing.easePointLinearly,
                     1,
                     .origin,
                     .{ .x = csz.getWidthFloat() * 0.75, .y = 0 },
@@ -98,7 +98,7 @@ pub fn event(ctx: jok.Context, e: jok.Event) !void {
                 try point_easing_system.add(
                     &screenshot_size,
                     easing.getEasingFn(.out_bounce),
-                    easing.easePoint,
+                    easing.easePointLinearly,
                     1,
                     csz.toPoint(),
                     csz.toPoint().scale(0.2),
@@ -107,7 +107,7 @@ pub fn event(ctx: jok.Context, e: jok.Event) !void {
                 try color_easing_system.add(
                     &screenshot_tint_color,
                     easing.getEasingFn(.in_out_quad),
-                    easing.easeColorF,
+                    easing.easeColorFLinearly,
                     1,
                     .none,
                     .white,

src/utils/easing.zig
@@ -290,27 +290,27 @@ pub fn easePointLinearly(t: f32, from: jok.Point, to: jok.Point, data: ?*anyopaq
     };
 }
 
-pub fn easeColorLinearly(t: f32, _from: jok.Color, _to: jok.Color, _: ?*anyopaque) jok.Color {
+pub fn easeColorLinearly(t: f32, _from: jok.Color, _to: jok.Color, data: ?*anyopaque) jok.Color {
     const es = EaseVectorLinearly(4, u8);
     const from = @Vector(4, u8){ _from.r, _from.g, _from.b, _from.a };
     const to = @Vector(4, u8){ _to.r, _to.g, _to.b, _to.a };
-    const c = es.ease(t, from, to);
+    const c = es.ease(t, from, to, data);
     return .{ .r = c[0], .g = c[1], .b = c[2], .a = c[3] };
 }
 
-pub fn easeColorFLinearly(t: f32, _from: jok.ColorF, _to: jok.ColorF, _: ?*anyopaque) jok.ColorF {
+pub fn easeColorFLinearly(t: f32, _from: jok.ColorF, _to: jok.ColorF, data: ?*anyopaque) jok.ColorF {
     const es = EaseVectorLinearly(4, f32);
     const from = @Vector(4, f32){ _from.r, _from.g, _from.b, _from.a };
     const to = @Vector(4, f32){ _to.r, _to.g, _to.b, _to.a };
-    const c = es.ease(t, from, to);
+    const c = es.ease(t, from, to, data);
     return .{ .r = c[0], .g = c[1], .b = c[2], .a = c[3] };
 }
 
-pub fn easeArrayLinearly(t: f32, comptime N: usize, _from: [N]f32, _to: [N]f32, _: ?*anyopaque) [N]f32 {
+pub fn easeArrayLinearly(t: f32, comptime N: usize, _from: [N]f32, _to: [N]f32, data: ?*anyopaque) [N]f32 {
     const es = EaseVectorLinearly(N, f32);
     const from: @Vector(N, f32) = _from;
     const to: @Vector(N, f32) = _to;
-    return es.ease(t, from, to);
+    return es.ease(t, from, to, data);
 }
 
 ///////////////////////////////////////////////////////////////////////

src/utils/spatial_hash.zig
@@ -83,14 +83,8 @@ pub fn SpatialHash(comptime ObjectType: type, opt: SpatialOption) type {
                 return self.put(obj, new_bounds);
             }
 
-            // Fast path: if new bounds still fully contained in old cells
-            const old_bounds = old_bounds_opt.?;
-            if (old_bounds.containsRect(new_bounds)) {
-                self.positions.put(obj, new_bounds) catch unreachable;
-                return;
-            }
-
             // Full update needed
+            const old_bounds = old_bounds_opt.?;
             try self.fillIntersectingCells(old_bounds);
             for (self.temp_cells.items) |cell_idx| {
                 const bucket = &self.buckets[cell_idx];